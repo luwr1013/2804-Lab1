e23e8f9433bafb09bedd8de5c8b34cb2d55ba6e4
Lukas Wright <103236840+luwr1013@users.noreply.github.com>
2023-06-09 13:16:09 -0600

    Update vehicleMilestone1.m
    code kinda works

--- vehicleMilestone1.m
+++ vehicleMilestone1.m
@@ -1,4 +1,4 @@
-
+clc; close all; clear all;
 
 constFuselage = calcFuselageDerivedInputs();
 S_wet_body = zeros(3,1);
@@ -12,63 +12,71 @@ Cd0_misc = zeros(3,1);
 Cd0_tot = zeros(3,1);
 
 for i=1:3
-S_wet_body(i) =2*pi*constFuselage.radius*constFuselage.length;
-cf_body(i) = .455/((log10(constFuselage.Re))^2.58*(1+(.144*constFuselage.Mach)^2)^.65);
-FF_body(i) = .9+5/(constFuselage.f)^1.5+constFuselage.f/400;
-cf_wing(i) = 0;
-FF_wing(i) = (1+(.6/constFuselage.Xc)*(constFuselage.t/constFuselage.c)+100*(constFuselage.t/constFuselage.c)^4)*((1.35*constFuselage.Mach)^.18*(cos(constFuselage.lambda))^.28);
-S_wetwing(i) = 0;
-Dq(i) = (.139+.419*(constFuselage.Mach-.161)^2)*constFuselage.A_base;
-Cd0_misc(i) = Dq/constFuselage.S_ref;
-Cd0_tot(i) = ((cf_body*FF_body*S_wet_body)+(FF_wing*constFuselage.Q_wing*cf_wing*S_wetwing))/constFuselage.S_ref+Cd0_misc;
+S_wet_body(i) =2.*pi.*constFuselage.radius(i).*constFuselage.length(i); % wetted area of body (m^2)
+cf_body(i) = .455./((log10(constFuselage.Re(i))).^2.58 .* (1+(.144.*constFuselage.Mach(i)).^2).^.65); % skin friction over fuselage (assume all turbulent)
+FF_body(i) = .9+5./(constFuselage.f(i)).^1.5+constFuselage.f(i)./400; % fuselage form factor
+cf_wing(i) = .074 ./ ((constFuselage.ReWing(i)).^.2); % skin friction over wing (assume all turbulent)
+FF_wing(i) = (1+(.6./constFuselage.Xc(i)).*(constFuselage.t(i)./constFuselage.c(i))+100.*(constFuselage.t(i)./constFuselage.c(i)).^4).*((1.35*constFuselage.Mach(i)).^.18.*(cos(constFuselage.lambda(i))).^.28);
+S_wetwing(i) = 2 .* constFuselage.wingPlanformArea(i); % wetted area of wing (m^2)
+% Dq(i) = (.139+.419.*(constFuselage.Mach(i)-.161).^2).*constFuselage.A_base(i);
+% Cd0_misc(i) = Dq./constFuselage.S_ref(i);
+Cd0_tot(i) = ((cf_body(i)*FF_body(i)*S_wet_body(i))+(FF_wing(i)*constFuselage.Q_wing(i)*cf_wing(i)*S_wetwing(i)))/constFuselage.S_ref(i);
 end
+
+%% Function
 function constFuselage = calcFuselageDerivedInputs()% this function calculates all the derived inputs needed for the fuselage
 %% Defining Given Constants
 % Standard Atmosphere (assume sea level)
 constFuselage.alt = [0, 4000, 13000]; % altitude(m)
 for i=1:3
-[T[i], constFuselage.a[i], P[i], constFuselage.rho[i]] = atmoscoesa(constFuselage.alt[i]); % temp, speed of sound, pressure, density
+[T(i), constFuselage.a(i), P(i), constFuselage.rho(i)] = atmoscoesa(constFuselage.alt(i)); % temp, speed of sound, pressure, density
 end
 
 constFuselage.vel = [21,63.9,260.8]; % velocity(m/s)
 constFuselage.length = [1.56,8.33,70.7]; % length(m)
 constFuselage.mu = [1.7894*(10^(-5)),1.6612*(10^(-5)),1.4216*(10^(-5))]; % viscosity(kg/m*s)
-constFuselage.k = 0; % surface whatever
+constFuselage.k = [.052 * 10^-5, .634*10^-5, .634*10^-5]; % surface k value
 constFuselage.Amax = [0.02,1.13,33.18]; % max area(m^2)
 constFuselage.radius=[0.08,0.6,3.25]; % radius(m)
 constFuselage.Q_body=[1,1,1];
-constFuselage.X = 0;
-constFuselage.c = 0;
-constFuselage.Xc=constFuselage.X/constFuselage.c;
-constFuselage.t = 0;
-constFuselage.lambda= [0, 0, 37.5]0;%sweep angle(degrees)
-constFuselage.Q_wing = 0;
-constFuselage.S_ref = 0;
-constFuselage.A_base=0;
+constFuselage.c = [.23,1.472,9.27]; % chord length (m) (cessna and 747 average chord)
+constFuselage.Xc= [.302,.3,.4]; % normalized chord position of max thickness
+constFuselage.t = [constFuselage.c(1) * .087, constFuselage.c(2) * .12,constFuselage.c(3) * .101]; % airfoil max thickness
+constFuselage.lambda= [0, 0, 37.5]; % sweep angle (degrees)
+constFuselage.Q_wing = [1.4, 1.4, 1.4]; % Q value for wing, assumed 1.4 for all since values unknown, this is upper limit so conservative estimate
+constFuselage.Width=[.16, 1.2, 6.5]; % width of fuselage
+constFuselage.bodyPlanformArea= constFuselage.Width .* constFuselage.length;
+constFuselage.wingPlanformArea = [.63, 16.17, 511]; %(m^2)
+constFuselage.S_ref = constFuselage.bodyPlanformArea + constFuselage.wingPlanformArea; % area of .Wiplane's shadow (m^2)
+% constFuselage.A_base=0; none of the planes have an aggressively sloped
+% aft end
+
 constFuselage.Wingspan=[3.22,11,59.6]; % m
-%% Calculating reynolds number
+%% Calculating fuselage reynolds number
 
-Re_1 = (constFuselage.rho * constFuselage.vel * constFuselage.length) / constFuselage.mu;
+for i = 1:3
+Re_1 = (constFuselage.rho(i) .* constFuselage.vel(i) .* constFuselage.length(i)) ./ constFuselage.mu(i);
 
-Re_cutoff = 38.21 * (constFuselage.length / constFuselage.k)^1.053; 
+Re_cutoff = 38.21 * (constFuselage.length(i) / constFuselage.k(i))^1.053; 
 
 if Re_1 < Re_cutoff
-    constFuselage.Re = Re_1;
+    constFuselage.Re(i) = Re_1;
 else 
-    constFuselage.Re = Re_cutoff;
+    constFuselage.Re(i) = Re_cutoff;
 end
+
+%% Calculating wing reynolds number
+
+constFuselage.ReWing(i) = (constFuselage.rho(i) .* constFuselage.vel(i) .* constFuselage.c(i)) ./ constFuselage.mu(i);
 
 %% Calculating the fineness ratio
 
-constFuselage.f = constFuselage.length / sqrt((4 * constFuselage.Amax) / pi);
+constFuselage.f(i) = constFuselage.length(i) / sqrt((4 * constFuselage.Amax(i)) / pi);
 
 %% Calculating Mach
 
-constFuselage.Mach = constFuselage.vel / constFuselage.a;
-
-%% Calculating S_wet
-
- % wetted area estimate, m^2
+constFuselage.Mach(i) = constFuselage.vel(i) / constFuselage.a(i);
 
 
+end
 end
